b6e44b7e584acff6fd92ec93215afb83dc76a87b
Mesi21 <mse212002@gmail.com>
2022-10-12 12:19:01 +0300

    :construction: add game logic of one round based on choices

--- game.js
+++ game.js
@@ -2,3 +2,33 @@ function computerPlay() {
   let choices = ["Rock", "Paper", "Scissors"];
   return choices[Math.floor(Math.random() * 3)];
 }
+
+function oneRound(playerSelection, computerSelection) {
+  let currPlayerSelection = playerSelection.toLowerCase().toUpperCase();
+  if(playerSelection === computerSelection) {
+    return "It\'s a tie";
+  } else {
+    if(playerSelection == "Rock") {
+      if(computerSelection == "Scissors") {
+        return "You win! Rock beats Scissors!";
+      } else {
+        return "You lose! Paper beats Rock!";
+      }
+    } else if(playerSelection == "Paper") {
+      if(computerSelection == "Rock") {
+        return "You win! Paper beats Rock!";
+      } else {
+        return "You lose! Scissors beat Paper!";
+      }
+    } else if(playerSelection == "Scissors") {
+      if(computerSelection == "Paper") {
+        return "You win! Scissors beat Paper!";
+      } else {
+        return "You lose! Rock beats Scissors!";
+      }
+    } else {
+      return "That is not a valid choice. Please make a new one!"
+    }
+  }
+}; 
+
